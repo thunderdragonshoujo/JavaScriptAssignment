f09ae1ab4f39563a758430316e34a198542e6e53
Clint McClure <thunderdragonshoujo@gmail.com>
2017-10-24 16:01:08 -0500

    FUNCTIONAL

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,10 @@
 const _ = require("lodash")
 console.log(
-    numberArry =_.range(0, 100).map(function(CurrentVale, index) {
-      index++;
-      if (index % 15 == 0){return "FizzBuzz";}
-      if (index % 3 == 0){return "Fizz";}
-      if (index % 5 == 0){return "Buzz";}
-      return index;
-    }).join('\n')
+    numberArry =_.filter(_.range(0, 100).map(function(index) {
+      
+      if (index % 15 == 0){return index +  " FizzBuzz";}
+      if (index % 3 == 0){return index + " Fizz";}
+      if (index % 5 == 0){return index + " Buzz";}
+      return null ;
+    }),null)
   );
